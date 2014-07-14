968eaa4e16fd22c68c91b34e623218d26977d549
Melanie <melanie.vanderlugt@gmail.com>
2014-07-14 14:42:06 +0700

    Refactor Deck Class.

--- 1_the_solitaire_cipher/solitaire.rb
+++ 1_the_solitaire_cipher/solitaire.rb
@@ -7,79 +7,84 @@ class Deck
 		@deck = (1..52).to_a << "A" << "B"
 	end
 
-	def move_a
-		a_index = deck.index("A")
-		move_down(a_index)
+	def get_key
+		move_a
+		move_b
+		triple_cut
+		count_cut
+		get_output_letter
 	end
 
-	def move_b
-		2.times do 
-			b_index = deck.index("B")
-			move_down(b_index)
-		end
-	end
+	private
 
-	def triple_cut
-		a_index = deck.index("A")
-		b_index = deck.index("B")
-		top_joker = [a_index, b_index].min
-		bottom_joker = [a_index, b_index].max
-		origdeck = deck.dup
-		deck.replace(origdeck[(bottom_joker+1)..-1].concat(origdeck[top_joker..bottom_joker]).concat(origdeck[0..(top_joker-1)]))
-	end
-
-	def count_cut
-		if deck.last == "A"
-			count = 53
-		elsif deck.last == "B"
-			count = 53
-		else
-			count = deck.last
-		end
-		origdeck = deck.dup
-		topcut = origdeck[0..(count-1)]
-		middle = origdeck[count..(deck.length-2)]
-		last = deck.last
-
-		deck.replace([middle, topcut, last].flatten)
-	end
-
-	def output_letter
-		if deck.first == "A" || deck.first == "B"
-			output_card = deck[53]
-		else
-			output_card = deck[deck.first]
+		def move_a
+			move_down(deck.index("A"))
 		end
 
-		puts "Deck first: #{deck.first}"
-		puts "Deck: #{deck}"
-		puts "Output card: #{output_card}"
-
-		if output_card == "A" || output_card == "B"
-			nil
-		else
-			to_letter(output_card)
+		def move_b
+			2.times do 
+				move_down(deck.index("B"))
+			end
 		end
-	end
 
-	def move_down(index)
-		if index == deck.length-1
-			deck.rotate!(deck.length-1)
-			index = 0
+		def triple_cut
+			top_joker = [deck.index("A"), deck.index("B")].min
+			bottom_joker = [deck.index("A"), deck.index("B")].max
+			topcut = deck[0..(top_joker-1)]
+			middle = deck[top_joker..bottom_joker]
+			bottomcut = deck[(bottom_joker+1)..-1]
+			deck.replace([bottomcut,middle,topcut].flatten)
 		end
-		deck[index], deck[index+1] = deck[index+1], deck[index]
-	end
+
+		def count_cut
+			if is_joker(deck.last)
+				count = 53
+			else
+				count = deck.last
+			end
+			topcut = deck[0..(count-1)]
+			middle = deck[count..(deck.length-2)]
+			last = deck.last
+			deck.replace([middle, topcut, last].flatten)
+		end
+
+		def get_output_letter
+			if is_joker(deck.first)
+				output_card = deck[53]
+			else
+				output_card = deck[deck.first]
+			end
+
+			if is_joker(output_card)
+				nil
+			else
+				to_letter(output_card)
+			end
+		end
+
+		def move_down(index)
+			if index == deck.length-1
+				deck.rotate!(deck.length-1)
+				index = 0
+			end
+			deck[index], deck[index+1] = deck[index+1], deck[index]
+		end
+
+		def is_joker(card)
+			if card == "A" || card == "B"
+				true 
+			else
+				false
+			end
+		end
 end
+
 
 def generate_keystream(length)
 	keydeck = Deck.new
 	keystream = []
 	while keystream.length < length do 
-		keydeck.move_a
-		keydeck.move_b
-		keydeck.triple_cut
-		keydeck.count_cut
-		new_key = keydeck.output_letter
+		new_key = keydeck.get_key
 		keystream << new_key unless new_key.nil?
 	end
 	keystream
