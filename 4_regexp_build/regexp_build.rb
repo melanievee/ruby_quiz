0ff340d270792e47f516870e9abe404c187785cc
Melanie <melanie.vanderlugt@gmail.com>
2014-07-31 17:57:00 +0700

    Initial solution, no optimization for ranges.

--- 4_regexp_build/regexp_build.rb
+++ 4_regexp_build/regexp_build.rb
@@ -2,3 +2,41 @@
 # build() should accept a variable number of arguments which can include integers and ranges 
 # of integers. Have build() return a Regexp object that will match only integers in the set 
 # of passed arguments.
+
+def Regexp.build( *args )
+	args.map! { |x| Array(x) }
+	# Regexp.union(args)
+	regex = "^(" + args.join('|') + ")"
+	Regexp.new regex
+end
+
+
+lucky = Regexp.build( 3, 7 )
+puts "lucky: #{lucky}"
+puts "Result of 7: #{!("7" =~ lucky).nil?}"
+puts "Result of 13: #{!("13" =~ lucky).nil?}"
+puts "Result of 3: #{!("3" =~ lucky).nil?}"
+
+month = Regexp.build( 1..12 )
+puts "month: #{month}"
+puts "Result of 0: #{!("0" =~ month).nil?}"
+puts "Result of 1: #{!("1" =~ month).nil?}"
+puts "Result of 5: #{!("5" =~ month).nil?}"
+puts "Result of 12: #{!("12" =~ month).nil?}"
+
+day = Regexp.build( 1..31 )
+puts "day: #{day}"
+puts "Result of 6: #{!("6" =~ day).nil?}"
+puts "Result of 16: #{!("16" =~ day).nil?}"
+puts "Result of Tues: #{!("Tues" =~ day).nil?}"
+
+year = Regexp.build( 98, 99, 2000..2005 )
+puts "year: #{year}"
+puts "Result of 04: #{!("04" =~ year).nil?}"
+puts "Result of 2004: #{!("2004" =~ year).nil?}"
+puts "Result of 99: #{!("99" =~ year).nil?}"
+
+num = Regexp.build( 0..1_000_000 )
+# puts "num: #{num}"
+puts "Result of -1: #{!("-1" =~ num).nil?}"
+puts "Result of 1000: #{!("1000" =~ num).nil?}"
